23d3ad0f9df761d1ab5bb7990dcdb823c2d6e9a7
Tomasz Wojciech Sałata <salata@artemis.wszib.edu.pl>
2022-06-07 19:09:49 +0200

    Poprawa ekranu pomocy

--- Trojkaty.java
+++ Trojkaty.java
@@ -9,6 +9,7 @@ class Trojkaty {
      * @param {float} b - Długość drugiego boku.
      * @param {float} c - Długość trzeciego boku.
      */
+
     public static void jakiTrojkat(float a, float b, float c){
          if (a == b && b == c && a == c) {
              System.out.println("Trójkąt równoboczny"); 
@@ -20,18 +21,21 @@ class Trojkaty {
 
          // TODO: tutaj trzeba bedzie dopisac inne przypadki
     }
-    /** Wyświetla ekran pomocy */
+/** Wyświetla ekran pomocy */
+
     public static void pomoc(){
         System.out.println("Acme INC. (C) 2022");
         System.out.println("Program do rozpoznawania rodzaju trójkąra");
         System.out.println("Uruchom z trzema argumentami liczbowymi - długość boków trójkąta");
     }
-    /** Glowna funkcja */
+/** Glowna funkcja */
+
     public static void main(String... args) {
         if (args.length != 3) {
             pomoc();
             System.exit(1);
         }
+
         float a = Float.valueOf(args[0]);
         float b = Float.valueOf(args[1]);
         float c = Float.valueOf(args[2]);
